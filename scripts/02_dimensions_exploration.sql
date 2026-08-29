8489ddd8dcf6a54020d8139019f2cd0e0c067356
exmondyeboah22 <exmondyeboah43@gmail.com>
2026-08-29 17:21:44 +0000

    Create 02_dimensions_exploration.sql

---
+++ scripts/02_dimensions_exploration.sql
@@ -0,0 +1,26 @@
+/*
+===============================================================================
+Dimensions Exploration
+===============================================================================
+Purpose:
+    - To explore the structure of dimension tables.
+	
+SQL Functions Used:
+    - DISTINCT
+    - ORDER BY
+===============================================================================
+*/
+
+-- Retrieve a list of unique countries from which customers originate
+SELECT DISTINCT 
+    country 
+FROM gold.dim_customers
+ORDER BY country;
+
+-- Retrieve a list of unique categories, subcategories, and products
+SELECT DISTINCT 
+    category, 
+    sub_category, 
+    product_name 
+FROM gold.dim_products
+ORDER BY category, sub_category, product_name;
